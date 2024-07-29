bf6cc57a14dad1bfd940c600e2d8ccb6d149bc9c
simriti07 <156352667+simriti07@users.noreply.github.com>
2024-07-29 16:37:28 +0530

    Create Directory.java

---
+++ Ex1/Structural Design/FileSystem/Directory.java
@@ -0,0 +1,41 @@
+package filesystem;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author DELL
+ */
+public class Directory implements FileSystemComponent {
+    private String name;
+    private List<FileSystemComponent> components = new ArrayList<>();
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+    public void addComponent(FileSystemComponent component) {
+        components.add(component);
+    }
+
+    public void removeComponent(FileSystemComponent component) {
+        components.remove(component);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Directory: " + name);
+        for (FileSystemComponent component : components) {
+            component.showDetails();
+        }
+    }
+
+    @Override
+    public long getSize() {
+        long totalSize = 0;
+        for (FileSystemComponent component : components) {
+            totalSize += component.getSize();
+        }
+        return totalSize;
+    }
+}
